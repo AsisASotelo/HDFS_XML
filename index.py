fc3b8cdd8c191f0d24db0d0a4564ee128f6c9241
AsisASotelo <asis.a.sotelo@gmail.comm>
2020-04-27 22:51:22 -0700

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,20 @@
 # Asis A Sotelo
 # April 27, 2020
 
-# index.py 
+# index.py
+
+import sys
+import lxml 
+import json
+
+
+
+# Argument Parser for Command Line Input takes in FSIMAGE
+# file name and INDEX file output file name
+
+
+
+# Production of index file for names of files and dir
+# Strings are assumed to be tokenized by white spaces
 
 
